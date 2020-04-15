5197777ed8af0808d851591b9c817c6862990497
zerix <zerix@zerix.net>
2020-04-15 13:18:32 -0400

    updating modules and using emojis

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Updates: ",	"~/.local/bin/pacupdate",	900,	0},
-	{" ",			"~/.local/bin/volume",	3,	0},
-	{" ",			"~/.local/bin/kernel",	36000, 0},
-	{" ",			"~/.local/bin/memory",	5,	0},
-	{" ",			"~/.local/bin/clock",	5,	0},
+	{"",			"~/.local/bin/weather",	1800,	0},
+	{" 🔄 ",		"~/.local/bin/pacupdate",	900,	0},
+	{" 🔊 ",			"~/.local/bin/volume",	3,	0},
+	{" 💻 ",			"~/.local/bin/kernel",	36000, 0},
+	{" 💾 ",			"~/.local/bin/memory",	5,	0},
+	{" ⏱️ ",			"~/.local/bin/clock",	5,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
