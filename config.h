5745b53cd78eb282696667512da0801895de7726
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2021-01-24 14:54:39 -0500

    Add files via upload

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",			"~/.local/bin/lukeweather",	1800,	0},
-	{" 🔄 ",		"~/.local/bin/pacupdate",	900,	0},
-	{" 🔊 ",			"~/.local/bin/volume",	3,	0},
-	{" 💻 ",			"~/.local/bin/kernel",	36000, 0},
-	{" 💾 ",			"~/.local/bin/memory",	5,	0},
-	{" 🗓 ",			"~/.local/bin/clock",	5,	0},
+	{"",			"~/.local/bin/weather",		1800,		0},
+	{"",			"~/.local/bin/pacupdate",	900,		0},
+	{"",			"~/.local/bin/volume",		3,			0},
+	{"",			"~/.local/bin/kernel",		36000,	0},
+	{"",			"~/.local/bin/memory",		5,			0},
+	{"",			"~/.local/bin/clock",		5,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
